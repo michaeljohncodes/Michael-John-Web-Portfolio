2eb4e06b12b9f7f1f6a0ebce8a9943f28724c323
michaeljohncodes <michaelchukwunyem@gmail.com>
2025-08-21 15:38:34 +0100

    projects

--- src/layouts/Projects.jsx
+++ src/layouts/Projects.jsx
@@ -7,60 +7,16 @@ import piggy2 from "../assets/piggy-mobile.png";
 const Projects = () => {
   return (
     <div className=" flex-col flex gap-30 max-tablet:gap-22 my-[80px]  max-mobile:px-4">
-      <div className="cursor-pointer width flex justify-between gap-[20px] max-tablet:flex-col">
-        <div className="max-w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3 max-mobile:max-w-[70%]">
-          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">
-            01
-          </h2>
-          <p className="font-semibold text-[#00FD9A] text-[30px] max-mobile:text-[24px]">
-            Netflix Clone
-          </p>
-          <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
-            Cloned a Real life website, to build more knowledge on how Pro Devs
-            create websites.
-          </p>
-          <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
-            <p>React.js,</p>
-            <p>Tailwind.css,</p>
-            <p>React State</p>
-          </div>
-          <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
-          <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
-            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
-              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
-                <BsArrowUpRight size={23} />
-              </div>
-            </a>
-            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
-              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
-                <FaGithub size={23} />
-              </div>
-            </a>
-          </div>
-        </div>
-        <div
-          className=" min-w-[50%] flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0  max-mobile:max-w-[70%]"
-          onClick={() =>
-            window.open("https://movie-api-delta-dun.vercel.app/", "_blank")
-          }
-        >
-          <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
-            <img src={piggy} alt="" />
-          </div>
-          <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
-            <img src={piggy2} alt="" />
-          </div>
-        </div>
-      </div>
-
-      <div className="cursor-pointer width flex justify-between gap-[20px] max-tablet:flex-col">
+      <div className="cursor-pointer width flex justify-between items-center gap-[20px] max-tablet:flex-col">
         <div className="max-w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3">
-          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">
-            02
-          </h2>
-          <p className="font-semibold text-[#00FD9A] text-[30px] max-mobile:text-[24px]">
-            PiggyVest
-          </p>
+          <div className=" flex gap-[30px] items-center">
+            <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">
+              01
+            </h2>
+            <p className="font-semibold text-[#00FD9A] text-[40px] max-mobile:text-[24px]">
+              PiggyVest
+            </p>
+          </div>
           <p className=" w-[100%] leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
             Cloned a Real life website, to build more knowledge on how Pro Devs
             create websites. Lorem ipsum dolor sit amet, consectetur adipisicing
@@ -78,12 +34,15 @@ const Projects = () => {
           </div>
           <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
           <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
-            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+            <a href="https://piggyvest-orpin.vercel.app/ " target="_blank">
               <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
                 <BsArrowUpRight size={23} />
               </div>
             </a>
-            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
+            <a
+              href="https://github.com/michaeljohncodes/Piggyvest"
+              target="_blank"
+            >
               <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
                 <FaGithub size={23} />
               </div>
@@ -93,7 +52,7 @@ const Projects = () => {
         <div
           className=" min-w-[50%] flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%]  h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0  max-mobile:max-w-[70%]"
           onClick={() =>
-            window.open("https://movie-api-delta-dun.vercel.app/", "_blank")
+            window.open("https://piggyvest-orpin.vercel.app/", "_blank")
           }
         >
           <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
@@ -104,6 +63,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
+
     </div>
   );
 };
@@ -111,5 +71,5 @@ const Projects = () => {
 export default Projects;
 // https://piggyvest-orpin.vercel.app/                         https://github.com/michaeljohncodes/Piggyvest
 // https://calculator-chi-eight-53.vercel.app/
-// https://moviesite-ivory-pi.vercel.app/
+// https://moviesite-ivory-pi.vercel.app/          https://github.com/michaeljohncodes/moviesite
 // https://moove-mu.vercel.app/
